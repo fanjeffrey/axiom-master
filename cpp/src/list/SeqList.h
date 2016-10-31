392c78a187c3701a48982d88c16df2c8ce842620
Jeffrey Fan <fanjeffrey@users.noreply.github.com>
2016-10-31 15:58:16 +0800

    separate main from template implementation (#3)

--- cpp/src/list/SeqList.h
+++ cpp/src/list/SeqList.h
@@ -1,4 +1,4 @@
-#if !defined SeqList_H
+#if !defined(SeqList_H)
 #define SeqList_H
 
 #include "LinearList.h"
@@ -13,14 +13,112 @@ class SeqList : public LinearList <T>
         virtual void Init();
         virtual int Length();
         virtual T Get(int index);
-        virtual T* GetData();
+        virtual T* Elements();
         virtual int Locate(T value);
         virtual void Insert(T value, int index);
         virtual T Delete(int index);
-        virtual void Display();
     protected:
-        T data[Capacity];
+        T elements[Capacity];
         int length;
 };
+
+template <class T>
+SeqList<T>::SeqList()
+{
+    T elements[Capacity];
+    length = 0;
+}
+
+template <class T>
+void SeqList<T>::Init()
+{
+    length = 0;
+}
+
+template <class T>
+int SeqList<T>::Length()
+{
+    return length;
+}
+
+template <class T>
+T SeqList<T>::Get(int index)
+{
+    if (length == 0)
+    {
+        throw "The list is empty.";      
+    }
+    
+    if (index < 1 || index > length)
+    {
+        throw "No data at this position.";    
+    }
+    
+    return elements[index - 1];
+}
+
+template <class T>
+T* SeqList<T>::Elements()
+{
+    return elements;
+}
+
+template <class T>
+int SeqList<T>::Locate(T value)
+{
+    for(int i=0; i<length; i++)
+    {
+        if (elements[i] == value) return i;
+    }
+    
+    return -1;
+}
+
+template <class T>
+void SeqList<T>::Insert(T value, int index)
+{    
+    if (length >= Capacity)
+    {
+        throw "The list is overflow.";
+    }
+    
+    if (index < 1 || index > length + 1)
+    {
+        throw "Wrong position for insertion.";
+    }
+    
+    for (int i = length - 1; i >= index; i--)
+    {
+        elements[i + 1] = elements[i];
+    }
+    
+    elements[index - 1] = value;
+    length ++;
+}
+
+template <class T>
+T SeqList<T>::Delete(int index)
+{    
+    if (length == 0)
+    {
+        throw "The list is empty.";
+    }
+    
+    if (index < 1 || index > length)
+    {
+        throw "Wrong position for deletion.";
+    }
+    
+    T temp = elements[index - 1];
+    
+    for (int i = index - 1; i < length - 1; i++)
+    {
+        elements[i] = elements[i+1];
+    }
+    
+    length --;
+    
+    return temp;
+}
 
 #endif
