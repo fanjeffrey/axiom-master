e869696f8a1c6f9829ca03dfefb5448c4fcf5409
Jeffrey F <jiangzhu.fan@outlook.com>
2016-11-11 19:59:40 +0800

    add SeqQueue

---
+++ cpp/src/queue/Queue.h
@@ -0,0 +1,20 @@
+#if !defined(Queue_H)
+#define Queue_H
+
+#include <string>
+
+using namespace std;
+
+template <typename T>
+class Queue
+{
+  public:
+    virtual void Enqueue(T &e) = 0;
+    virtual void Dequeue() = 0;
+    virtual T &GetFront() = 0;
+    virtual bool IsEmpty() = 0;
+    virtual bool IsFull() = 0;
+    virtual string ToString() = 0;
+};
+
+#endif
